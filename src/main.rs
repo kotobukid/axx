6ee82d0c795c2bd79d342a01bb758f1a0d167766
kotobukid <kotobuki@daqua.jp>
2023-06-13 00:13:31 +0900

    Axumサンプル実行

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
-fn main() {
-    println!("Hello, world!");
-    println!("{}", greet("Taro".into()));
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root));
+    let addr = SocketAddr::from(([192, 168, 33, 10], 3000));
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
 
-fn greet (name: String) -> String {
-    format!("hello {}", &name)
+async fn root() -> &'static str {
+    "Hello, world"
 }
