c1a252afc4869ca1f655bb61ea483eea7626f648
kotobukid <kotobuki@daqua.jp>
2023-06-18 23:39:36 +0900

    書籍に従いTodoListのCRUDを途中まで実装 todo!()マクロの存在を知る

--- myaxum/server/src/auth/mod.rs
+++ myaxum/server/src/auth/mod.rs
@@ -1,10 +1,13 @@
-use axum:: {
+use std::collections::HashMap;
+use std::sync::{Arc, RwLock};
+use axum::{
     response::{Html, IntoResponse},
     extract::Form,
     Json,
     http::{StatusCode},
 };
 use serde::{Deserialize, Serialize};
+use thiserror::Error;
 
 pub async fn create_user(Json(payload): Json<CreateUser>) -> impl IntoResponse {
     let user = User {
@@ -43,4 +46,83 @@ pub struct CreateUser {
 pub struct User {
     pub id: u64,
     pub username: String,
+}
+
+#[derive(Debug, Error)]
+enum RepositoryError {
+    #[error("NotFound, id is {0}")]
+    NotFound(i32),
+}
+
+pub trait TodoRepository: Clone + std::marker::Send + std::marker::Sync + 'static {
+    fn create(&self, payload: CreateTodo) -> Todo;
+    fn find(&self, payload: CreateTodo) -> Todo;
+    fn all(&self) -> Vec<Todo>;
+    fn update(&self, id: i32, payload: UpdateTodo) -> anyhow::Result<Todo>;
+    fn delete(&self, id: i32) -> anyhow::Result<()>;
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+pub struct Todo {
+    id: i32,
+    text: String,
+    completed: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+pub struct CreateTodo {
+    test: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+pub struct UpdateTodo {
+    text: Option<String>,
+    completed: Option<bool>,
+}
+
+impl Todo {
+    pub fn new(id: i32, text: String) -> Self {
+        Self {
+            id,
+            text,
+            completed: false,
+        }
+    }
+}
+
+type TodoDatas = HashMap<i32, Todo>;
+
+#[derive(Debug, Clone)]
+pub struct TodoRepositoryForMemory {
+    store: Arc<RwLock<TodoDatas>>,
+}
+
+impl TodoRepositoryForMemory {
+    pub fn new() -> Self {
+        TodoRepositoryForMemory {
+            store: Arc::default(),
+        }
+    }
+}
+
+impl TodoRepository for TodoRepositoryForMemory {
+    fn create(&self, payload: CreateTodo) -> Todo {
+        todo!();
+    }
+
+    fn find(&self, id: i32) -> Option<Todo> {
+        todo!();
+    }
+
+    fn all(&self) -> Vec<Todo> {
+        todo!();
+    }
+
+    fn update(&self, id: i32, payload: UpdateTodo) -> anyhow::Result<Todo> {
+        todo!();
+    }
+
+    fn delete(&self, id: i32) -> anyhow::Result<()> {
+        todo!();
+    }
 }
